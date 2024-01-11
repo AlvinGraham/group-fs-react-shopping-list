f181b984021aff81cf6d9c9f239a040070797f8e
Alvin Graham <grah0071@gmail.com>
2024-01-11 10:17:21 -0600

    Create initial database and to do list

--- database.sql
+++ database.sql
@@ -1,2 +1,13 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE shopping_items (
+	id SERIAL PRIMARY KEY,
+	name varchar(80) NOT NULL,
+	quantity decimal NOT NULL,
+  unit varchar(20),
+  purchased boolean
+);
+
+INSERT INTO "shopping_items" ("name", "quantity", "unit", "purchased")
+VALUES ('Bananas', 2, 'bunch', false), ('Apples', 6, 'each', false);
