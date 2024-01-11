76cf70c72bc06bcc1751670e757dcdaeb8ead503
Alvin Graham <grah0071@gmail.com>
2024-01-11 10:56:37 -0600

    Add and test server GET route

--- server/routes/itemList.router.js
+++ server/routes/itemList.router.js
@@ -1,3 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
+
+// GET Routes
+router.get('/', (req, res) => {
+
+  // GET query Text
+  const queryText = `SELECT * FROM "shopping_items" ORDER BY "name";`;  
+  // DB Query
+  pool.query(queryText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.error('ERROR in GET DB Query:', err);
+      res.sendStatus(500);
+    });
+});
+
+// POST Routes
+
+
+// UPDATE Routes
+
+
+// DELETE Routes
+
+module.exports = router;
