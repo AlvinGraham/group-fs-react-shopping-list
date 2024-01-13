d7aae9da6765ff3ef478b7516fbc3f08208aa15a
Alvin Graham <grah0071@gmail.com>
2024-01-13 13:48:34 -0600

    Add and test client GET route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
-
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import Header from '../Header/Header.jsx'
 import './App.css';
 import AddItemForm from '../AddItemForm/AddItemForm.jsx';
 
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <AddItemForm />
-        </div>
-    );
+  const [shoppingList, setShoppingList] = useState([]);
+
+  const refreshShoppingList = () => {
+    axios.get('/api/itemList')
+      .then((response) => {
+        setShoppingList(response.data);
+        console.log('Data Received from DB:');
+        console.table(response.data);
+      })
+      .catch((err) => {
+        console.error('Error in client GET:', err);
+      });
+  };
+
+  useEffect(() => {
+    refreshShoppingList();
+  }, []);
+
+
+
+  return (
+    <div className="App">
+      <Header />
+      <AddItemForm />
+    </div>
+  );
 }
 
 export default App;
