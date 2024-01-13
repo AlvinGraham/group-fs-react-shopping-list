ab71b203700000edfdd73eb060f5c69798105b0b
Alvin Graham <grah0071@gmail.com>
2024-01-13 15:17:15 -0600

    Add styling to Cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react';
+import './Card.css';
 
 export default function Card (props) {
-  console.log(props.cardData);
+  // console.log(props.cardData);
   return (
-    <>
-     <h3>I am Card {props.cardData.name}</h3>
-     <button>Buy</button>
-     <button>Remove</button>
-    </>
+    
+    <div className='card'>
+      <p>{props.cardData.name}</p>
+      <p>{props.cardData.quantity} {props.cardData.unit}</p>
+      <div className='buttonField'>
+        <button>Buy</button>
+        <button>Remove</button>
+        </div>
+    </div>
+    
   );
 }//{props.shoppingItem.name}
--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Card from '../Card/Card';
+import './ShoppingList.css';
 
 export default function ShoppingList (props) {
   console.log('shoppingListArray:');
@@ -11,9 +12,11 @@ export default function ShoppingList (props) {
       <h2>Shopping List:</h2>
       <button>Reset</button>
       <button>Clear</button>
-      {(props.shoppingListArray).map ((shoppingItem) => {
-        return <Card cardData={shoppingItem} />
-      })}
+      <div className='cardField'>
+        {(props.shoppingListArray).map ((shoppingItem, itemIndex) => {
+          return <Card key={itemIndex} cardData={shoppingItem} />
+        })}  
+      </div>
     </div>
   );
 }
